44c59425c9ef191dff228874a1c8381a07fb2bf4
Titilayor01 <oyedeletitilayov@mail.com>
2023-03-21 09:41:41 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print all alphabet in lowercase
+ *Return: return 0
+ */
+
+void print_alphabet(void)
+{
+char a_z;
+for (a_z = 'a'; a_z <= 'z'; a_z++)
+{
+_putchar(a_z);
+}
+_putchar('\n');
+return (0);
+
