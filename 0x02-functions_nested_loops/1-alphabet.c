bf87fb4ca6f9563556db7da56b9ee34aeb6c3348
Titilayor01 <oyedeletitilayov@mail.com>
2023-03-21 10:00:55 -0700

    hot fix file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *print_alphabet - print all alphabet in lowercase
- *Return: return 0
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0;
+ *
  */
-
 void print_alphabet(void)
 {
-char a_z;
-for (a_z = 'a'; a_z <= 'z'; a_z++)
-{
-_putchar(a_z);
-}
-_putchar('\n');
-return (0);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
