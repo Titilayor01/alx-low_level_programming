f23cc19d82babfb2be12c2b9f449a01aa2aeb889
Titilayor01 <oyedeletitilayov@mail.com>
2023-04-03 06:32:04 -0700

    created a mainh file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+int_putchar(char);
+char *memset(char *s, char b, unsigned int n);
+char *memcpy(char *dest, char *src,unsigned int n);
+char *strchr(char *s, char c);
+unsigned int _strspn(char *s char *accept);
+char *strpbrk(char *s char *accept);
+char *strstr(char *haystack char *needle);
+void print_chessboard(char (a*)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
