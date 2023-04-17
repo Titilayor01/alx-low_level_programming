ed2579fbfaa91ad1e39a7b078c1ce3372c81b99a
Titilayor01 <oyedeletitilayov@mail.com>
2023-04-17 04:52:08 -0700

    a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+// a program that prints the name of the file it was compiled with
+
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: 0
+ */
+int main()
+{
+	printf("s%\n", __FILE__);
+
+	return 0
+
+}
